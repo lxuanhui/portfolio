7b7339038f6ac3889a6f236fd6f192ed867d9a9a
Lee Xuanhui <xuanhui98@gmail.com>
2023-02-06 21:40:39 +0800

    Added about me

--- portfolio/src/components/aboutme.js
+++ portfolio/src/components/aboutme.js
@@ -23,16 +23,20 @@ const AboutMe = () => (
                         />
                         </div></div>
                         <div class="row">
+                            
                             <div class="col-4">
 
-                                <div class="portfoliopic" data-aos="fade-up" data-aos-delay="100"><img src="./assets/images/portfoliopic.PNG" alt=""/></div>
+                                <div class="portfoliopic" data-aos="fade-up" data-aos-delay="100"><img src="google.jpg" alt="Potrait"/></div>
                             </div>
                             <div class="col-8" data-aos="fade-up" data-aos-delay="100">
                 
-                                <div class="service border-hover p-5 shadow rounded-5 bg-dark" >
-                                    <h5 class="fw-bold">Senior Computer Science Major at Nanyang Technological University</h5>
+                                <div class="text-bubble service border-hover p-5 shadow rounded-5 bg-dark" >
+                                    <h2 class="fw-bold">Senior Computer Science Major at Nanyang Technological University</h2>
                                     
                                     <p class="mb-0">Graduating in December 2023!</p>
+                                </div>
+								<div class="text-bubble service border-hover p-5 shadow rounded-5 bg-dark" >
+                                    <h2 class="fw-bold">Participated in an exchange semester at the University of Maryland</h2>
                                 </div>
                             </div>
                         </div>
